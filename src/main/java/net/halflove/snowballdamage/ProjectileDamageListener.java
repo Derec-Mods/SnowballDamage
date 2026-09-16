7e9f84011ea2347acd59528f23ad5cf1ddff3826
Derek <derex1987@gmail.com>
2026-09-16 01:38:27 -0500

    feat: why did we make that so complicated lmao

--- src/main/java/net/halflove/snowballdamage/ProjectileDamageListener.java
+++ src/main/java/net/halflove/snowballdamage/ProjectileDamageListener.java
@@ -1,6 +1,5 @@
 package net.halflove.snowballdamage;
 
-import org.bukkit.EntityEffect;
 import org.bukkit.entity.Egg;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Projectile;
@@ -9,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.util.Vector;
 
 public class ProjectileDamageListener implements Listener {
 
@@ -23,28 +21,13 @@ public class ProjectileDamageListener implements Listener {
             return;
         }
 
-        LivingEntity victim = (LivingEntity) event.getHitEntity();
-        Vector direction = projectile.getVelocity().clone();
-        direction.setY(0);
-        if (direction.lengthSquared() < 1.0E-6) {
-            return;
-        }
-        direction.normalize().multiply(0.4);
-
-        Vector current = victim.getVelocity();
-        victim.setVelocity(new Vector(
-                current.getX() / 2.0 + direction.getX(),
-                victim.isOnGround() ? 0.4 : current.getY(),
-                current.getZ() / 2.0 + direction.getZ()
-        ));
-        victim.playEffect(EntityEffect.HURT);
+        ((LivingEntity) event.getHitEntity()).damage(0.0, projectile);
     }
 
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Snowball || event.getDamager() instanceof Egg) {
             event.setDamage(0.0);
-            event.setCancelled(true);
         }
     }
 }
